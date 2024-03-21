83e1a9d3a9fa6e9e87b555ed2196cf60f7ebd96e
CupponeManuel <5a@PianoDue-04>
2024-03-21 09:07:53 +0100

    versione 0:errata

---
+++ src/CupponeManuelClass.java
@@ -0,0 +1,40 @@
+
+/**
+ * 
+ * @author <b>CupponeManuel</b>
+ * <dl>4AI</dl>
+ * <i>21/03/2024</i>
+ *
+ */
+
+public class CupponeManuelClass {
+
+
+	/**
+	 * @param Metodo Costruttore
+	 */
+	public CupponeManuelClass() {
+		
+	}
+	
+	/**
+	 * 
+	 * @param a
+	 * @param e
+	 * @param Prende come parametri 2 interi e calcola quanto vale il parametro1 elevato all parametro2
+	 * @return
+	 */
+	
+	public int cupponeMet(int a, int e) {
+		int v=0; 
+		for(int i=0; i<=e; i++)
+			v+=v*a;
+		return v;
+	}
+	
+	public static void main(String[] args) {
+		CupponeManuelClass a=new CupponeManuelClass();
+		System.out.println(a.cupponeMet(8, 4));
+	}
+	
+}
